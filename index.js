63918c55a8bae3c0b90d9b1b3111dd1433c19fcb
Matt <matt.larter@me.com>
2020-11-28 19:51:14 -0400

    Setting logging for Rotation speed

--- index.js
+++ index.js
@@ -302,6 +302,7 @@ TosotHeaterCooler.prototype = {
     getRotationSpeed: function (callback) {
         let speed = this.device.getFanSpeed();
         speed = speed === commands.fanSpeed.value.auto ? 6 : speed;
+        this.log.debug('Triggered GET Rotation Speed:' value);
 
         callback(null, speed);
 
@@ -310,6 +311,7 @@ TosotHeaterCooler.prototype = {
         if (this._isContextValid(context)) {
             let speed = RotationSpeed === 6 ? commands.fanSpeed.value.auto : RotationSpeed;
             this.device.setFanSpeed(speed);
+            this.log.debug('Triggered SET Rotation Speed:' value);
         }
         callback();
     },
